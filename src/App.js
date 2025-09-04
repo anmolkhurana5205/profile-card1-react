8cb25095ed88086666b030b05329acb3bda7fbac
anmolkhurana5205 <anmol.2327cse1235@kiet.edu>
2025-09-04 21:50:33 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="container">
+        <Header />
+        <Data />
+        <Skills />
+      </div>
     </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      src="/Snapchat-1300530555.jpg"
+      alt="profile"
+      height="250px"
+      className="avatar"
+    />
+  );
+}
+
+function Header() {
+  return <h1>Anmol Khurana</h1>;
+}
+
+function Data() {
+  return (
+    <p className="data">
+      Good morning/afternoon. My name is Anmol Khurana, and I am currently
+      pursuing Bachelor of Technology in Computer Science and Engineering at
+      Krishna Institute of Engineering & Technology, with an expected graduation
+      in 2027.
+    </p>
+  );
+}
+
+function Skills() {
+  return (
+    <div className="skill-list">
+      <Skill skillName="HTML 😖" color="blue" />
+      <Skill skillName="JAVASCRIPT 🫡" color="red" />
+      <Skill skillName="CSS 🫠" color="yellow" />
+      <Skill skillName="JAVA 🤫" color="lightgreen" />
+      <Skill skillName="PYTHON 😱" color="purple" />
+      <Skill skillName="REACT 😩" color="orange" />
+      <Skill skillName="NODE 😡" color="lightblue" />
+      <Skill skillName="EXPRESS 🤤" color="brown" />
+      <Skill skillName="NEXT 😶" color="gray" />
+    </div>
+  );
+}
+
+function Skill({ skillName, color }) {
+  return (
+    <span style={{ backgroundColor: color }} className="skill">
+      {skillName}
+    </span>
   );
 }
 
